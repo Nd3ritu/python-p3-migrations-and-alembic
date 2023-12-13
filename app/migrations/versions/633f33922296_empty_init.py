634e419ff8027dce78df79b3135a32cdd69a12fc
Matthew <+>
2023-12-13 14:39:46 +0300

    migrations and alembic

---
+++ app/migrations/versions/633f33922296_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 633f33922296
+Revises: 
+Create Date: 2023-12-13 12:30:00.487887
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '633f33922296'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
